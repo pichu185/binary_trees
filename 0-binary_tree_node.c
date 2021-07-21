1b4a082fb6bd1024daebc52af78cc65c3ea9e09b
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-20 21:51:44 -0300

    0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Entry point
+ * @parent: is a pointer to the parent node of the node to create
+ * @value:  is the value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
+ */
+ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+ {
+	binary_tree_t *new = NULL;
+
+	new = malloc(sizeof(binary_tree_t))
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	new->n = value;
+	return (new);
+ }
+ 
