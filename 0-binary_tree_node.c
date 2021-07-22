8af90531a511ab5e3c0aa4195b81b56006ef33f8
pichu185 <77989459+pichu185@users.noreply.github.com>
2021-07-22 14:28:08 -0300

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,17 +8,17 @@
  */
  binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
  {
-	binary_tree_t *new = NULL;
+        binary_tree_t *new = NULL;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (!new)
-	{
-		return (NULL);
-	}
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
-	new->n = value;
-	return (new);
+        new = malloc(sizeof(binary_tree_t));
+        if (!new)
+        {
+                return (NULL);
+        }
+        new->parent = parent;
+        new->left = NULL;
+        new->right = NULL;
+        new->n = value;
+        return (new);
  }
  
